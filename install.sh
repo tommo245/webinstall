f50602f6f114e7c119a13f9e819429f06d200191
tommo245 <tommo245@gmail.com>
2010-03-30 14:31:27 +0100

    first attempt at gitting this list of p[ackages

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+aptitude update
+aptitude -y safe-upgrade
+
+# install PHP
+apt-get install -y php5-cli
+
+# install PHP-dev
+# need some tools including phpize
+apt-get install -y php5-dev
+
+# Install apache
+aptitude -y install libapache2-mod-wsgi
+
+# Install PEAR
+# PEAR needs to be >=1.8.0 for PHING + dependencies, above cmd only gets 1.7.x
+# so...upgrade
+apt-get -y install php-pear
+pear upgrade PEAR
+
+# Install Subversion
+apt-get -y install subversion
+
+# Install Phing
+# Used for deploying stuff
+pear channel-discover pear.phing.info
+pear install --alldeps phing/phing
+
+# Install Cruise
+# and deal with licenses
